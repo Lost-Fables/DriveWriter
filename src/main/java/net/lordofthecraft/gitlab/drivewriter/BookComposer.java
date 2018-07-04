9147e85e1226dde01a4cca975a31bb50c35779bb
Colin <pokemonlove1389@gmail.com>
2018-07-04 11:54:42 -0400

    Switched to Jsoup (Html Parsers ftw)
    Signed-off-by: Colin <pokemonlove1389@gmail.com>

--- src/main/java/net/lordofthecraft/gitlab/drivewriter/BookComposer.java
+++ src/main/java/net/lordofthecraft/gitlab/drivewriter/BookComposer.java
@@ -2,9 +2,12 @@ package net.lordofthecraft.gitlab.drivewriter;
 
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -33,11 +36,23 @@ public class BookComposer {
 
     }
 
-    private String driveReader(){
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"))) {
-            for (String line; (line = reader.readLine()) != null;) {
-                System.out.println(line);
-            }
+    private Document documentGetter(URL url){
+        Document document = null;
+        try {
+            document = Jsoup.connect(url.toString()).get();
+        }catch (IOException error){
+            player.sendMessage("Error, IO issue getting the document from the provdied URL");
+            safe = false;
+        }
+        return document;
+    }
+
+    private String textGetter(Document document){
+        String output;
+
+        Elements text = document.select("p");
+        for (Element element : text){
+            element.text().toString();
         }
     }
 }
